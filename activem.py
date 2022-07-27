1b42b321fd8b6356902c94d28dd4739619228987
Owen <butlero2001@outlook.com>
2022-07-27 16:10:10 -0400

    Updated output logic and removed unnecessary plotting of initial frame

--- activem.py
+++ activem.py
@@ -13,22 +13,19 @@ def animate(t, swarm):
     [ax.quiver(bot.pos[0], bot.pos[1], bot.uvec[0], bot.uvec[1], width = 0.003) for bot in swarm.botlist]
 
 """ Main Body """
-plot = False
+save = False
 numbots = 10
 arena = Arena(5, 5)
 swarm = Swarm(arena, numbots, Bot)
 
-if(plot):
+if(save):
+    aniframes = 300
     fig = plt.figure(figsize = (12, 9))
     ax = fig.add_subplot(111, xlim = (-arena.dw, arena.dw), ylim = (-arena.dh, arena.dh))
-    for bot in swarm.botlist:
-        ax.plot(bot.pos[0], bot.pos[1], 'bo')
-        ax.quiver(bot.pos[0], bot.pos[1], bot.uvec[0], bot.uvec[1], width = 0.003)
-    plt.show()
-
-if(not plot):
+    ani = FuncAnimation(fig, animate, fargs = (swarm,), interval = 10, frames = aniframes, repeat = False)
+    ani.save("animation.mp4", fps = 30)
+else:
     fig = plt.figure(figsize = (12, 9))
     ax = fig.add_subplot(111, xlim = (-arena.dw, arena.dw), ylim = (-arena.dh, arena.dh))
     ani = FuncAnimation(fig, animate, fargs = (swarm,), interval = 10, frames = None, repeat = False)
-    # ani.save("animation.mp4", fps = 30)
     plt.show()
