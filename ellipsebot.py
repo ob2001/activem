686ee6e01bd131e0311f4ebf5ed01e25efc9e266
Owen <butlero2001@outlook.com>
2022-08-08 12:36:02 -0400

    Added initialization

--- ellipsebot.py
+++ ellipsebot.py
@@ -2,4 +2,7 @@ from lib import *
 from bot import Bot
 
 class EllipseBot(Bot):
-    pass
+    def __init__(self, W, H, p_res, d_res):
+        self.pos = [randbw(W, p_res), randbw(H, p_res)]
+        self.uvec = [randbw(1, d_res), randbw(1, d_res)]
+        self.a, self.b = a, b
