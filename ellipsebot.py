302f7fc8fa511a480861e997d90fba891b3e5dd4
Owen <butlero2001@outlook.com>
2022-08-08 13:21:29 -0400

    Add funcctionality to EllipseBot

--- ellipsebot.py
+++ ellipsebot.py
@@ -2,7 +2,20 @@ from lib import *
 from bot import Bot
 
 class EllipseBot(Bot):
+    v = 0.08
     def __init__(self, W, H, p_res, d_res):
-        self.pos = [randbw(W, p_res), randbw(H, p_res)]
-        self.uvec = [randbw(1, d_res), randbw(1, d_res)]
-        self.a, self.b = a, b
+        self.pos = np.array([randbw(W, p_res), randbw(H, p_res)])
+        self.uvec = np.array([randbw(1, d_res), randbw(1, d_res)]) # Points in direction of major axis
+        self.normalizeuvec()
+        self.a, self.b = randbw(1, d_res), randbw(1, d_res)
+
+    # Renormalizes bot's uvec
+    def normalizeuvec(self):
+        self.uvec = self.uvec/np.linalg.norm(self.uvec)
+
+    # Get bot's ovec
+    def getovec(self):
+        return [[0, 1], [-1, 0]]@self.uvec
+
+    def collision(self, botb):
+        pass
