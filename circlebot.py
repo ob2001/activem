f23e9f35e84cf0f83f4a3ead223ba03331ccf704
Owen <butlero2001@outlook.com>
2022-08-08 12:35:50 -0400

    Moved collision and initialization code from swarm to bot class so they can be generalized

--- circlebot.py
+++ circlebot.py
@@ -5,12 +5,12 @@ from bot import Bot
 # access to its own position, direction, etc.
 class CircleBot(Bot):
     v = 0.08
+    collrad = 1.5
+    collang = np.pi
 
-    def __init__(self, pos, uvec, arena, xbound, ybound):
-        self.pos = pos
-        self.uvec = uvec
-        self.xbound, self.ybound = xbound, ybound
-        self.arena = arena
+    def __init__(self, W, H, p_res, d_res):
+        self.pos = [randbw(W, p_res), randbw(H, p_res)]
+        self.uvec = [randbw(1, d_res), randbw(1, d_res)]
 
         self.normalizeuvec()
 
@@ -28,3 +28,10 @@ class CircleBot(Bot):
     def rotate(self, theta, r):
         self.uvec += [r*np.cos(theta), r*np.sin(theta)]
         self.normalizeuvec()
+
+    def collision(self, botb):
+        d = distance(self.pos, botb.pos)
+        if(d < self.collrad and angle(self.uvec, vecdiffr(self.pos, botb.pos, 1)) < self.collang and sees(self.pos, self.uvec, self.collang, botb.pos)[0]):
+            vec = vecdiffr(self.pos, botb.pos, (self.collrad - d)/2)
+            botb.pos += vec
+            self.pos -= vec
