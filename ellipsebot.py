3a01af17d6fc8542bde4693273574b7fc5930a33
Owen <butlero2001@outlook.com>
2022-08-08 17:32:20 -0400

    Added name property. Can specify ellipse axis lengths. Added rotate(). Fixed getovec(). Added draw().

--- ellipsebot.py
+++ ellipsebot.py
@@ -1,21 +1,38 @@
 from lib import *
 from bot import Bot
+from matplotlib.patches import Ellipse
 
 class EllipseBot(Bot):
     v = 0.08
-    def __init__(self, W, H, p_res, d_res):
+    name = "EllipseBot"
+
+    def __init__(self, W, H, p_res, d_res, a = None, b = None):
         self.pos = np.array([randbw(W, p_res), randbw(H, p_res)])
         self.uvec = np.array([randbw(1, d_res), randbw(1, d_res)]) # Points in direction of major axis
         self.normalizeuvec()
-        self.a, self.b = randbw(1, d_res), randbw(1, d_res)
+        if(a is None and b is None):
+            self.a, self.b = randbw(1, d_res), randbw(1, d_res)
+        elif(a is not None and b is not None):
+            self.a, self.b = a, b
+        elif(a is not None):
+            self.a, self.b = a, randbw(1, d_res)
+        else:
+            self.a, self.b = randbw(1, d_res), b
 
     # Renormalizes bot's uvec
     def normalizeuvec(self):
         self.uvec = self.uvec/np.linalg.norm(self.uvec)
 
+    def rotate(self, theta):
+        self.uvec = rotvec(self.uvec, theta)
+        self.normalizeuvec()
+
     # Get bot's ovec
     def getovec(self):
-        return rotvec(-np.pi/2)@self.uvec
+        return rotvec(self.uvec, -np.pi/2)
 
     def collision(self, botb):
         pass
+
+    def draw(self, ax):
+        ax.add_patch(Ellipse((self.pos[0], self.pos[1]), self.a, self.b, angle = 90 + angfromuvecd(self.uvec)))
