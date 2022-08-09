c41afaa1214dc52d466bf891a922aef1d1dc4c10
Owen <butlero2001@outlook.com>
2022-08-09 16:51:18 -0400

    Made arena into an abstract class. Sqreflarena implements it

--- arena.py
+++ arena.py
@@ -1,34 +1,17 @@
 from lib import *
+from abc import ABC, abstractmethod
 
 # Class for the arena in which the bots are interacting.
-# Mainly just a container for x and y bounds
-class Arena:
-    def __init__(self, h, w):
-        self.h = h
-        self.w = w
-        self.boundsup()
-
+class Arena(ABC):
     # TODO: Boundary types:
     # TODO - Periodic
     # TODO - Solid
     # DONE - Reflective
 
-    # Update the variables used for plot bounds if/when the
-    # arena bounds are changed
+    @abstractmethod
     def boundsup(self):
-        self.dh, self.dw = self.h + self.h/10, self.w + self.w/10
+        pass
 
+    @abstractmethod
     def boundcoll(self, items):
-        for item in items:            
-            if(item.pos[0] < -self.w):
-                item.pos[0] = -self.w
-                item.uvec[0] *= -1
-            elif(item.pos[0] > self.w):
-                item.pos[0] = self.w
-                item.uvec[0] *= -1
-            if(item.pos[1] < -self.h):
-                item.pos[1] = -self.h
-                item.uvec[1] *= -1
-            elif(item.pos[1] > self.h):
-                item.pos[1] = self.h
-                item.uvec[1] *= -1
+        pass
