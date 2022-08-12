54527851f67e02ad1b13465a4e0ed5cc2d9a6279
Owen <butlero2001@outlook.com>
2022-08-12 17:07:13 -0400

    Made the update function a property of the individual bots.

--- Lib/swarm.py
+++ Lib/swarm.py
@@ -2,18 +2,17 @@ from Lib.Bot.bot import Bot
 from Lib.Arena.arena import Arena
 
 class Swarm:
-    def __init__(self, Arena: Arena, n, Bot: Bot, **kwargs):
+    def __init__(self, Arena: Arena, n, Bot: Bot, upf, upfargs, **kwargs):
         self.Arena = Arena
         self.n = n
         self.Bot = Bot
 
-        # Generate and return a list of bots with positions
-        # and directions having the specified resolutions
-        self.botlist = [self.Bot(self.Arena.w, self.Arena.h, kwargs) for _ in range(n)]
+        # Generate and return a list of bots with randomized positions and directions
+        self.botlist = [self.Bot(self.Arena.w, self.Arena.h, upf, upfargs, **kwargs) for _ in range(n)]
 
     # Function used when animating bots.
     # Updates bots and redraws on given axis.
-    def animate(self, t, ax, func, *args):
+    def animate(self, t, ax):
         # ** Use this to update the bounds of the arena while animating ** #
         # if(t == 100):
         #     self.Arena.w *= 5
@@ -22,21 +21,21 @@ class Swarm:
         ax.clear()
         ax.set_xlim(-self.Arena.dw, self.Arena.dw)
         ax.set_ylim(-self.Arena.dh, self.Arena.dh)
-        self.update(func, args)
+        self.update()
         self.draw(ax)
     
     # This function is called on every frame of animation
     # to update the bots' angles, perform movement, and
     # check collisions
-    def update(self, func, args):
-        self.updateuvec(func, args)
+    def update(self):
+        self.updateuvec()
         self.updatepos()
         self.docollisions()
 
     # Change the angle of the bot by a small random amount
-    def updateuvec(self, func, args):
+    def updateuvec(self):
         for bot in self.botlist:
-            bot.rotate(func(*args))
+            bot.rotate(bot.upf(*bot.upfargs))
 
     # Step the bot forward one unit of its velocity
     # in the direction of its uvec
@@ -59,5 +58,5 @@ class Swarm:
     def draw(self, ax):
         for bot in self.botlist:
             bot.draw(ax)
-        [ax.plot(bot.pos[0], bot.pos[1], 'bo') for bot in self.botlist]
-        [ax.quiver(bot.pos[0], bot.pos[1], bot.uvec[0], bot.uvec[1], width = 0.003) for bot in self.botlist]
+        for bot in self.botlist: ax.plot(bot.pos[0], bot.pos[1], 'bo')
+        for bot in self.botlist: ax.quiver(bot.pos[0], bot.pos[1], bot.uvec[0], bot.uvec[1], width = 0.003)
