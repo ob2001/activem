c988f48f395f1892288c06cb89ffb6fbe5463e2d
Owen <butlero2001@outlook.com>
2022-08-12 17:04:25 -0400

    Add a square solid-boundary arena

---
+++ Lib/Arena/sqsolarena.py
@@ -0,0 +1,19 @@
+from ..lib import np
+from .arena import Arena
+
+class SqSolArena(Arena):
+    def __init__(self, h, w):
+        self.h = h
+        self.w = w
+        self.boundsup()
+
+    def boundsup(self):
+        self.dh, self.dw = self.h + self.h/10, self.w + self.w/10
+
+
+    def boundcoll(self, items):
+        for item in items:            
+            if(item.pos[0] < -self.w): item.pos[0] = -self.w
+            elif(item.pos[0] > self.w): item.pos[0] = self.w
+            if(item.pos[1] < -self.h): item.pos[1] = -self.h
+            elif(item.pos[1] > self.h): item.pos[1] = self.h
