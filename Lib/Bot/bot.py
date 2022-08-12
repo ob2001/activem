deaeb290878c5ee70574d9858d740fbea8823a0f
Owen <butlero2001@outlook.com>
2022-08-12 17:04:43 -0400

    Moved some common functions into the parent Bot class

--- Lib/Bot/bot.py
+++ Lib/Bot/bot.py
@@ -1,15 +1,26 @@
 from abc import ABC, abstractmethod
 
+from ..lib import np, rotvec
+
 class Bot(ABC):
     @abstractmethod
     def __init__(self):
         pass
 
     @abstractmethod
-    def rotate(self, theta: float):
+    def collision(self, botb):
         pass
 
     @abstractmethod
-    def draw():
+    def draw(ax):
         """Method used to draw bot shape at correct position and with correct orientation, size, etc"""
         pass
+
+    # Rotate uvec by angle specified.
+    def rotate(self, theta):
+        self.uvec = rotvec(self.uvec, theta)
+        self.normalizeuvec()
+
+    # Renormalizes bot's uvec
+    def normalizeuvec(self):
+        self.uvec = self.uvec/np.linalg.norm(self.uvec)
