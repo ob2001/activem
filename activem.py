1e47fa5da6443a88ee382960a209e87d3c1c2415
Owen <butlero2001@outlook.com>
2022-08-12 12:57:42 -0400

    Fix import, update animation code

--- activem.py
+++ activem.py
@@ -1,4 +1,4 @@
-from lib import *
+from Lib.lib import *
 from Lib.swarm import Swarm
 from Lib.Bot.circlebot import CircleBot
 from Lib.Bot.ellipsebot import EllipseBot
@@ -10,7 +10,8 @@ from matplotlib.animation import FuncAnimation
 """ Main Body """
 save = False
 l, w, numbots = 5, 5, 10
-swarm = Swarm(SqReflArena(l, w), numbots, EllipseBot, a = 0.25, b = 1.5)
+swarm = Swarm(SqReflArena(l, w), numbots, CircleBot, a = 0.25, b = 1.5)
+anif, anifargs = random.gauss, (0.01, 0.2)
 
 if(save):
     aniframes = 300
@@ -21,5 +22,5 @@ if(save):
 else:
     fig = plt.figure(figsize = (10, 9))
     ax = fig.add_subplot(111, xlim = (-swarm.Arena.dw, swarm.Arena.dw), ylim = (-swarm.Arena.dh, swarm.Arena.dh))
-    ani = FuncAnimation(fig, swarm.animate, fargs = (ax,), interval = 10, frames = None, repeat = False)
+    ani = FuncAnimation(fig, swarm.animate, fargs = (ax, anif, *anifargs), interval = 10, frames = None, repeat = False)
     plt.show()
