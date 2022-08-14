a645dc346351b18767ef4215f3ddf31725003078
Owen <butlero2001@outlook.com>
2022-08-13 21:51:34 -0400

    Make the collision radius a property of the circle bot

--- Lib/Bot/circlebot.py
+++ Lib/Bot/circlebot.py
@@ -5,8 +5,9 @@ from matplotlib.patches import Circle
 # Class for each individual bot. Each one will have
 # access to its own position, direction, etc.
 class CircleBot(Bot):
+    """kwargs:
+    - r: sets circlebot collision radius"""
     v = 0.08
-    collrad = 1.0
     name = "CircleBot"
 
     def __init__(self, W, H, upf, upfargs, **kwargs):
@@ -15,14 +16,15 @@ class CircleBot(Bot):
         self.normalizeuvec()
         self.upf = upf
         self.upfargs = upfargs
+        self.r = kwargs['r']
 
     def collision(self, botb):
         collang = np.pi
         d = distance(self.pos, botb.pos)
-        if(d < self.collrad and angle(self.uvec, vecdiffr(self.pos, botb.pos, 1)) < collang and sees(self.pos, self.uvec, collang, botb.pos)[0]):
-            vec = vecdiffr(self.pos, botb.pos, (self.collrad - d)/2)
+        if(d < self.r and angle(self.uvec, vecdiffr(self.pos, botb.pos, 1)) < collang and sees(self.pos, self.uvec, collang, botb.pos)[0]):
+            vec = vecdiffr(self.pos, botb.pos, (self.r - d)/2)
             botb.pos += vec
             self.pos -= vec
 
     def draw(self, ax):
-        ax.add_patch(Circle((self.pos[0], self.pos[1]), self.collrad/2, fill = False, edgecolor = 'black'))
+        ax.add_patch(Circle((self.pos[0], self.pos[1]), self.r/2, fill = False, edgecolor = 'black'))
