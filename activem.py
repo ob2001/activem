c14e8225fd72d4c49a4f3133ac58d8770d71cf05
Owen <butlero2001@outlook.com>
2022-08-18 16:53:28 -0400

    Changed imports

--- activem.py
+++ activem.py
@@ -6,16 +6,16 @@ from matplotlib.animation import FuncAnimation
 
 from lib.fs import randbw
 from lib.swarm import Swarm
-from lib.arenas import SqPerArena, SqReflArena, SqSolArena
-from lib.bots import CircleBot, EllipseBot, PlainBot
+import lib.arenas as arenas
+import lib.bots as bots
 from lib.interaction import InteractionMethods as IM
 
 """ Main Body """
 dataout, animate, save = False, True, False
 l, w, numbots = 5, 5, 10
-arena = SqReflArena(l, w)
-botlist = [CircleBot(gauss, (0, 0.2), 0.5) for _ in range(numbots)]
-swarm = Swarm(arena, botlist, IM.ccollide)
+arena = arenas.SqPerArena(l, w)
+botlist = [bots.EllipseBot(gauss, (0, 0.2), 1.5, 0.25) for _ in range(numbots)]
+swarm = Swarm(arena, botlist, IM.ellcollide)
 
 if(save and animate):
     upframes = 300
@@ -25,12 +25,14 @@ if(save and animate):
     plot, quiver, shapes = swarm.ani_init(ax)
     ani = FuncAnimation(fig, swarm.animate, fargs = (ax, plot, quiver, shapes), interval = 10, frames = upframes, repeat = False)
     ani.save(ani_name, fps = 20)
+
 elif(animate):
     fig = plt.figure(figsize = (10, 9))
     ax = fig.add_subplot(111, xlim = (-swarm.Arena.dw, swarm.Arena.dw), ylim = (-swarm.Arena.dh, swarm.Arena.dh))
     plot, quiver, shapes = swarm.ani_init(ax)
     ani = FuncAnimation(fig, swarm.animate, fargs = (ax, plot, quiver, shapes), interval = 50, frames = None, repeat = False)
     plt.show()
+
 elif(dataout):
     frames = 2
     data = np.empty((numbots, frames))
