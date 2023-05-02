f0e54751d0d3b8a3c2c4d933f84eda28e21c5914
ob2001 <butlero2001@outlook.com>
2023-05-02 17:06:32 -0400

    Switched to Polygon for general polygons

--- lib/bot/tribot.py
+++ lib/bot/tribot.py
@@ -1,7 +1,7 @@
 from typing import Callable
 import numpy as np
 from matplotlib.pyplot import Axes
-from matplotlib.patches import Patch, RegularPolygon
+from matplotlib.patches import Patch, Polygon
 
 from ..fs import angfromuvec
 from .bot import Bot
@@ -17,10 +17,20 @@ class TriBot(Bot):
         self.normalizeuvec()
         self.upf = upf
         self.upfargs = upfargs
+        self.colliding = False
+        self.vertices = [[-0.1, 0], [0, 0.5], [0.1, 0], [0, -0.5]]
 
     def draw(self, ax: Axes) -> Patch:
-        return ax.add_patch(RegularPolygon(self.pos, 3, self.r, angfromuvec(self.uvec) - np.pi/2, fill = False))
+        ang = angfromuvec(self.uvec)
+        rotmat = np.array([[np.cos(ang), np.sin(ang)], [-np.sin(ang), np.cos(ang)]])
+        return ax.add_patch(Polygon(self.vertices@rotmat + self.pos, closed = True, fill = False))
 
     def redraw(self, shape: Patch):
-        shape.xy = self.pos
-        shape.orientation = angfromuvec(self.uvec) - np.pi/2
+        ang = angfromuvec(self.uvec) - np.pi/2
+        rotmat = np.array([[np.cos(ang), np.sin(ang)], [-np.sin(ang), np.cos(ang)]])
+        shape.xy = self.vertices@rotmat + self.pos
+        if self.colliding:
+            shape.set_edgecolor('r')
+            self.colliding = False
+        else:
+            shape.set_edgecolor('k')
