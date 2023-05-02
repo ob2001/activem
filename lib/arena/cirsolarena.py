7a54d5380bf013659d61c1a302895e6f0927ba57
ob2001 <butlero2001@outlook.com>
2023-05-02 17:02:18 -0400

    Add circular arenas

---
+++ lib/arena/cirsolarena.py
@@ -0,0 +1,22 @@
+from .arena import Arena
+from numpy import sqrt, array, pi, cos, sin
+from ..fs import vecdiffn, randbw
+
+class CirSolArena(Arena):
+    def __init__(self, r: float):
+        self.r = r
+        self.boundschanged = False
+        self.boundsup()
+
+    def boundsup(self):
+        self.dw = self.dh = self.r + 0.1*self.r
+        self.boundschanged = True
+
+    def boundcoll(self, items: list):
+        for item in items:
+            rpos = sqrt(item.pos[0]**2 + item.pos[1]**2)
+            if(rpos > self.r):
+                item.pos -= (rpos - self.r)*vecdiffn([0, 0], item.pos)
+
+    def randpos(self, bot):
+        bot.pos = randbw(0, self.r)*array([cos(randbw(0, 2*pi)), sin(randbw(0, 2*pi))])
--- lib/arenas.py
+++ lib/arenas.py
@@ -1,3 +1,5 @@
 from .arena.sqperarena import SqPerArena
 from .arena.sqreflarena import SqReflArena
 from .arena.sqsolarena import SqSolArena
+from .arena.cirreflarena import CirReflArena
+from .arena.cirsolarena import CirSolArena
